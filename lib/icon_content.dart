1299713dd75130426a29109433850289810749ce
umarrg <umarabubakarfta@gmail.com>
2020-11-28 21:03:38 +0100

    second commit

---
+++ lib/icon_content.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+const labelTextStyle = 
+TextStyle(fontSize: 18.0, color: Color(0xff8d8b98));
+class IconsContent extends StatelessWidget {
+  IconsContent({@required this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Icon(
+        icon,
+        size: 80.0,
+      ),
+      SizedBox(height: 15.0),
+      Text(
+        label,
+        style: labelTextStyle,
+      )
+    ]);
+  }
+}
---
+++ lib/input_page.dart
@@ -0,0 +1,105 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0XFF1D1E33);
+const bottomContainerColor = Color(0XFFEb1555);
+const inActiveCardColor = Color(0xff111328);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Color maleColor = inActiveCardColor;
+  Color femaleColor = inActiveCardColor;
+
+  void updateColor(int gender) {
+    //male card pressed
+    if (gender == 1) {
+      if (maleColor == inActiveCardColor) {
+        maleColor = activeCardColor;
+      }else {
+        maleColor = inActiveCardColor;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColor(1);
+                        });
+                        print('Male card was pressed');
+                      },
+                      child: ReusableCard(
+                        colour: activeCardColor,
+                        cardChild: IconsContent(
+                          icon: FontAwesomeIcons.mars,
+                          label: 'MALE',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                      cardChild: IconsContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            )),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            ),
+          ],
+        ));
+  }
+}
